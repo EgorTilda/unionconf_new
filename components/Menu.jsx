49722b92a72fdc34c15743d45d72a734eea854dd
Geo <egortilda@gmail.com>
2023-12-20 13:32:04 +0300

    mobile menu fix

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -3,11 +3,11 @@ const Menu = ({active, setActive}) => {
     return (
         <nav className={active ? 'menu active' : 'menu'}>
             <div className="menu__inner">
-                <a href="#main" onClick={() => close()} className="menu__link menu__link--first">Главная</a>
-                <a href="#about" onClick={() => close()} className="menu__link">О платформе</a>
-                <a href="#benefits" onClick={() => close()} className="menu__link">Преимущества</a>
-                <a href="#spaces" onClick={() => close()} className="menu__link">Локации</a>
-                <a href="#scale" onClick={() => close()} className="menu__link">Функционал</a>
+                <a href="#main" className="menu__link menu__link--first">Главная</a>
+                <a href="#about" className="menu__link">О платформе</a>
+                <a href="#benefits" className="menu__link">Преимущества</a>
+                <a href="#spaces" className="menu__link">Локации</a>
+                <a href="#scale" className="menu__link">Функционал</a>
             </div>
         </nav>
     );
